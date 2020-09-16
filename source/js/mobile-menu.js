75dcf9180c23caf51a7b9b1e7ac26cdf757bbe3f
Vilmarin <59697462+Vilmarin@users.noreply.github.com>
2020-09-16 11:57:09 +0300

    no-js

--- source/js/mobile-menu.js
+++ source/js/mobile-menu.js
@@ -2,6 +2,8 @@ const $header = document.querySelector('.page-header')
 const $buttonClose = $header.querySelector('.page-header__close-button')
 const $buttonOpen = $header.querySelector('.page-header__toggle')
 
+$header.classList.remove('no-js')
+
 $buttonClose.addEventListener('click', () => {
   $header.classList.remove('active')
 })
